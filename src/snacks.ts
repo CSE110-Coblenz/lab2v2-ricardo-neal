2899b824dcf24845b6c73279fd082a5993655931
Neal <nealc58@outlook.com>
2025-10-09 11:42:29 -0700

    m

---
+++ src/snacks.ts
@@ -0,0 +1,12 @@
+const snacks: string[] = [];
+
+snacks.push('ricecripy');
+snacks.push('chips');
+snacks.push('fruit');
+
+export function printSnacks(): void {
+    console.log("the snacks we're gonna have: ");
+    console.log(snacks);
+}
+
+printSnacks();
