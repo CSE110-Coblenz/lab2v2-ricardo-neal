19cb4e2bd58a4ca3e84d4fce395b7089625c9ffd
Neal <nealc58@outlook.com>
2025-10-09 17:16:46 -0700

    Added all final snacks

--- src/snacks.ts
+++ src/snacks.ts
@@ -3,6 +3,9 @@ const snacks: string[] = [];
 snacks.push('ricecripy');
 snacks.push('chips');
 snacks.push('fruit');
+snacks.push('cheese');
+snacks.push('nuts');
+snacks.push('ham');
 
 export function printSnacks(): void {
     console.log("the snacks we're gonna have: ");
